9ee10549d94fb7d5061f5f3085655568f3be9042
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-12-12 09:49:08 +0100

    include tmux

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+echo 'Moving ./nvim/init.vim to ~/.config/nvim/init.vim ...'
 cp ./nvim/init.vim ~/.config/nvim/init.vim
+
+echo 'Moving ./tmux/tmux.conf to ~/.tmux.conf ...'
+cp ./tmux/tmux.conf ~/.tmux.conf
+
+echo 'Done.'
+
