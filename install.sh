6784935afbdb0839a78221f9825b675911f49a7b
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-03-23 12:16:04 +0100

    Make install script create config directories

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+mkdir -p ~/.config/nvim
 cp ./init.vim ~/.config/nvim/init.vim
+
+mkdir -p ~/.config/kitty
 cp ./kitty.conf ~/.config/kitty/kitty.conf
+
 cp ./tmux.conf ~/.tmux.conf
 cp ./bashrc ~/.bashrc
 
