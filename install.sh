de9ff5d88de1308b2c0b52e09c971f769310f9d9
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-04-07 09:20:34 +0200

    Add bash profile

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ mkdir -p ~/.config/kitty
 cp ./kitty.conf ~/.config/kitty/kitty.conf
 
 cp ./tmux.conf ~/.tmux.conf
+
 cp ./bashrc ~/.bashrc
+cat ~/bash_profile && cp ./bash_profile ~/.bash_profile || cp ./bash_profile ~/.profile
 
 echo 'Done.'
