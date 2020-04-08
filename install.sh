f9479bcff4f0220a38bf11b78a7c6450ed1684ac
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-04-08 14:01:19 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ cp ./init.vim ~/.config/nvim/init.vim
 mkdir -p ~/.config/kitty
 cp ./kitty.conf ~/.config/kitty/kitty.conf
 
-cp ./tmux.conf ~/.tmux.conf
+cp ./.tmux.conf ~/.tmux.conf
 
-cp ./bashrc ~/.bashrc
-[ -e ~/.bash_profile ] && cp ./bash_profile ~/.bash_profile || cp ./bash_profile ~/.profile
+cp ./.bashrc ~/.bashrc
+[ -e ~/.bash_profile ] && cp ./.bash_profile ~/.bash_profile || cp ./.bash_profile ~/.profile
 
 echo 'Done.'
